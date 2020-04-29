35616eff4c29533330b5cdb0f100f7387fc959ee
Wayne <wayne.c.griffiths@gmail.com>
2020-04-29 14:46:10 +0800

    removed unused node_modules

--- app.js
+++ app.js
@@ -14,18 +14,18 @@ const Fn = require("./lib/functions.js");
 const prompts = new Rx.Subject();
 
 //creates our prompt module.
-inquirer.prompt(prompts).ui.process.subscribe(answer=>{
+inquirer.prompt(prompts).ui.process.subscribe(async answer=>{
     //check to see if the question had a function associated with it.
     if (typeof(Fn[answer.name])==="function"){
-        Fn[answer.name](answer.answer);//wait for the callback to complete
+        await Fn[answer.name](answer.answer);//wait for the callback to complete
     } else {
-        Fn.temp(answer);
+        await Fn.temp(answer); //answers that don't have a callback will be directed to the temp function, this stores the answer in the temp.data array
     }
-    Fn.checkQueue();
-    prompts.next(Fn.nextQuestion()); //otherwise, call the next question.
+    await Fn.checkQueue(); //checks queue status
+    prompts.next(Fn.nextQuestion()); //call the next question.
 },
     error => console.log(error),
-    complete => console.log(complete)
+    complete => console.log('complete')
 );
 
 //kicks off our prompt module with the first question.
